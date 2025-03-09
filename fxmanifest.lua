315a68f537904404e2919b429b3936d1ffc4a71d
FinnRP <52898375+FinnRP@users.noreply.github.com>
2025-03-08 23:17:26 -0600

    (tweak): Alter fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,18 +3,16 @@ game 'gta5'
 lua54 'yes'
 
 shared_scripts {
-    'shared/**.lua',
+    'shared/*.lua',
 }
 
 client_scripts {
     '@PolyZone/client.lua',
     '@PolyZone/BoxZone.lua',
-    '@PolyZone/ComboZone.lua',
     '@ox_lib/init.lua', -- Remove if not using ox_lib
-    '@qbx_core/modules/playerdata.lua',
-    'client/**.lua',
+    'client/*.lua',
 }
 
 server_scripts {
-    'server/**.lua'
+    'server/*.lua'
 }
